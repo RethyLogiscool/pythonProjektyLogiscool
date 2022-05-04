99a4c89b3ce2bc380beb0a8cce78e532da88b09c
RethyLogiscool <93673094+RethyLogiscool@users.noreply.github.com>
2022-05-04 18:54:19 +0200

    DvanactkaLekce

---
+++ dvanactaLekce2.py
@@ -0,0 +1,50 @@
+numbers = []
+for i in range(10):
+    #numbers.append(int(input("Přidej číslo do listu: ")))
+    print("")
+print(numbers)
+odd = 0
+even = 0
+positive = 0
+negative = 0
+zero = 0
+for num in numbers:
+    if num > 0:
+        positive += 1
+    elif num < 0:
+        negative += 1
+    else:
+        zero += 1
+    if num % 2 == 0:
+        even += 1
+    else:
+        odd += 1
+
+list = [1,2,3,4]
+print(list)
+list.extend([5,6,7])
+print(list)
+list.extend("Ahoj")
+print(list)
+
+list = [2,3,1.5,"Hello",-2]
+intList = []
+for i in list:
+    if type(i) == int:
+        intList.append(i)
+print(list)
+print(intList)
+
+list = [1, 5, 100, 2, 3, 8, 12, 1, 7, 4]
+print("Nejvyšší: " + str(max(list)) + ", 2. nejvyšší: " + str(max(list[:list.index(max(list))]+list[list.index(max(list))+1:])))
+
+maxNum = 0
+druMax = 0
+for num in list:
+    if num > maxNum:
+        maxNum = num
+print(maxNum)
+for num in list:
+    if num > druMax and num != maxNum:
+        druMax = num
+print(druMax)
