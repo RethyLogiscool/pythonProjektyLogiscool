cd8e8f2e173386fcb6e9e13919ef234e09833190
RethyLogiscool <93673094+RethyLogiscool@users.noreply.github.com>
2022-05-11 19:17:49 +0200

    trinactkaLekce

--- trinactaLekce2.py
+++ trinactaLekce2.py
@@ -1,12 +1,12 @@
 import random, time
 
-
 list = []
 maxNum = 10000
 for i in range(maxNum, 0, -1):
-    list.append(i)
-print(list)
-
+    rndNum = random.randint(1,maxNum)
+    while rndNum in list:
+        rndNum = random.randint(1,maxNum)
+    list.append(rndNum)
 
 
 def selectionSort(unsortedList):
@@ -15,14 +15,13 @@ def selectionSort(unsortedList):
         for j in range(i+1, len(unsortedList)):
             if unsortedList[min_id] > unsortedList[j]:
                 min_id = j
-
-        unsortedList[i],unsortedList[min_id] = unsortedList[min_id], unsortedList[i]
+        unsortedList[i], unsortedList[min_id] = unsortedList[min_id],unsortedList[i]
     return unsortedList
 
 def bubbleSort(unsortedList):
     for i in range(len(unsortedList)-1, 0, -1):
         for j in range(i):
-            if unsortedList[j] > unsortedList[j+1]:
+            if unsortedList[j] > unsortedList [j+1]:
                 unsortedList[j], unsortedList[j+1] = unsortedList[j+1], unsortedList[j]
     return unsortedList
 
@@ -36,17 +35,34 @@ def insertionSort(unsortedList):
         unsortedList[j+1] = key
     return unsortedList
 
-start = time.time()
-selectionSort(list)
-print("Selection sort:")
-print(time.time() - start)
-
-start = time.time()
-bubbleSort(list)
-print("Bubble sort:")
-print(time.time() - start)
 
 start = time.time()
 insertionSort(list)
-print("Insertion sort:")
+print("Insertion sort: ")
 print(time.time() - start)
+
+list = []
+for i in range(maxNum, 0, -1):
+    rndNum = random.randint(1,maxNum)
+    while rndNum in list:
+        rndNum = random.randint(1,maxNum)
+    list.append(rndNum)
+
+start = time.time()
+selectionSort(list)
+print("Selection sort: ")
+print(time.time() - start)
+
+list = []
+for i in range(maxNum, 0, -1):
+    rndNum = random.randint(1,maxNum)
+    while rndNum in list:
+        rndNum = random.randint(1,maxNum)
+    list.append(rndNum)
+
+start = time.time()
+bubbleSort(list)
+print("Bubble sort: ")
+print(time.time() - start)
+
+
