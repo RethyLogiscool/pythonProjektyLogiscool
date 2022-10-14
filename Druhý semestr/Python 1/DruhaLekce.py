a10b2f011d2052a36252b15e82c7e1ddb79b251d
ondrarethy <logiscool@rethy.cz>
2022-10-14 16:39:50 +0200

    druha lekce (nahrada)

---
+++ Druhý semestr/Python 1/DruhaLekce.py
@@ -0,0 +1,20 @@
+print("Jak se máš?".__len__())
+
+
+
+splitString = "Dnes bude svíčková;Zítra bude koprovka".split(";")
+print(splitString)
+
+for string in splitString:
+    print(string)
+
+print("Jak se máš janku".upper().count("J"))
+
+print("Jak se máš?".find("se"))
+
+print("Jak se máš?".startswith("Jak"))
+print("Jak se máš?".endswith("?"))
+
+print("Jak".isalpha())
+print("10".isdecimal())
+
